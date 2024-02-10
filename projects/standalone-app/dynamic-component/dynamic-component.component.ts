69513c6bcaa5aab2de1cb6d6a757904979a1002c
Thiago Guterman <thiago2k9@gmail.com>
2024-02-10 13:45:44 -0300

    feat(adding support to standalone apps, angular/cdk)

---
+++ projects/standalone-app/dynamic-component/dynamic-component.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dynamic-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dynamic-component.component.html',
+  styleUrls: ['./dynamic-component.component.scss']
+})
+export class DynamicComponentComponent implements OnInit {
+
+  @Output() destroyToast: EventEmitter<boolean> = new EventEmitter<boolean>(false);
+  context!: {
+    content: string
+  };
+
+  constructor() { }
+
+  rate(rate: number): void {
+    console.log(rate);
+    this.destroyToast.emit(true);
+  }
+
+  ngOnInit(): void {
+    console.log('Dynamically injected data:')
+    console.log(this.context.content);
+  }
+
+}
